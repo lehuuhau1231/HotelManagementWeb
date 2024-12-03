de872f27925dc749fd616aeb6cdcab5794fc246a
unknown <lamn9049@gmail.com>
2024-12-03 23:50:20 +0700

    innit

--- app/dao.py
+++ app/dao.py
@@ -6,11 +6,15 @@ import cloudinary.uploader
 
 def auth_user(username,password):
     password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
-    return User.query.filter(User.username.__eq__(username) and User.password.__eq__(password)).first()
+    return User.query.filter(User.username.__eq__(username), User.password.__eq__(password)).first()
 
 
 def get_user_by_id(user_id):
     return User.query.get(user_id)
+
+
+def get_user_by_account(account):
+    return User.query.filter(User.username.__eq__(account) or User.email.__eq__(account)).first()
 
 
 def add_user(name, username, password, email, phone, avatar, gender, identification, type):
@@ -30,3 +34,11 @@ def add_user(name, username, password, email, phone, avatar, gender, identificat
 
 def existence_check(attribute ,value):
     return User.query.filter(getattr(User, attribute).__eq__(value)).first()
+
+def change_password(user_id, new_password):
+    new_password = str(hashlib.md5(new_password.strip().encode('utf-8')).hexdigest())
+    user = get_user_by_id(user_id)
+    if user:
+        user.password = new_password
+        db.session.commit()
+
--- app/index.py
+++ app/index.py
@@ -1,16 +1,24 @@
 import re
-from flask import render_template, request, redirect, flash
+from datetime import date
+from warnings import catch_warnings
+
+from flask import render_template, request, redirect, flash, session
 from app import app, dao, login_manager
 from flask_login import login_user, logout_user
+import smtplib
+import random
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    date_today = date.today().strftime('%Y-%m-%d')
+    print(date_today)
+    return render_template('index.html', date_today=date_today)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    err_message = ''
     if request.method.__eq__('POST'):
         username = request.form.get('username')
         password = request.form.get('password')
@@ -19,8 +27,10 @@ def login():
         if user:
             login_user(user)
             return redirect('/')
+        else:
+            err_message = 'username or password incorrect'
 
-    return render_template('login.html')
+    return render_template('login.html', err_message=err_message)
 
 
 @app.route('/logout')
@@ -72,7 +82,7 @@ def register():
             return render_template('register.html', identification_card=identification_card,
                                    error_message=error_message, name=name, username=username_value,
                                    email=email_value
-                                   ,phone=phone_value, customer_type=customer_type, gender=gender)
+                                   , phone=phone_value, customer_type=customer_type, gender=gender)
         else:
             data = request.form.copy()
             del data['confirm']
@@ -89,6 +99,64 @@ def load_user(user_id):
     return dao.get_user_by_id(user_id)
 
 
+@app.route('/forgot-password', methods=['GET', 'POST'])
+def forgot_password():
+    err_message = ''
+    step = int(request.form.get('step', '1'))
+    print(step)
+
+    if request.method.__eq__('POST'):
+        if step == 1:
+            account = request.form.get('account')
+            user = dao.get_user_by_account(account)
+            session['user_id'] = user.id
+            if user:
+                send_email(user)
+                return render_template('forgotPassword.html', step=2)
+            else:
+                err_message = 'username or email do not exist'
+
+        elif step == 2:
+            otp_code = request.form.get('otp')
+            otp_code_sent = session.get('otp_code')
+            if int(otp_code.strip()) == int(otp_code_sent):
+                print("success step 2")
+                return render_template('forgotPassword.html', step=3)
+            else:
+                err_message = 'OTP code do not match'
+
+        elif step == 3:
+            print("start step 2")
+            session.pop('otp_code', None)  # Giải phóng bộ nhớ
+            user_id = session.get('user_id')
+            print(user_id)
+            password = request.form.get('password')
+            confirm_password = request.form.get('confirm')
+            if password.__eq__(confirm_password):
+                dao.change_password(user_id=user_id, new_password=password)
+                flash('Please login', 'Changed password successfully')
+                return redirect('/login')
+            else:
+                err_message = 'Password and confirm password do not match'
+
+    return render_template('forgotPassword.html', err_message=err_message, step=step)
+
+
+def send_email(user):
+    email_sender = 'lehuuhau005@gmail.com'
+    session['otp_code'] = str(random.randint(100000, 999999))
+    message = f"Hello {str(user.name)}\n\nVerification code:{session['otp_code']}\n\nThanks,"
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(email_sender, 'wsja hdjk nfvn boih')
+        server.sendmail(email_sender, user.email, message.encode('utf-8'))
+    except Exception as e:
+        print("Send mail ERROR: ", e)
+    finally:
+        server.quit()
+
+
 @app.route('/roomdetail')
 def roomdetail():
     return render_template('roomdetail.html')
@@ -101,22 +169,32 @@ def booking():
 
 @app.route('/nvxemphong')
 def nvxemphong():
-    return render_template('nvxemphong.html')
+    return render_template('employees/nvxemphong.html')
 
 
 @app.route('/nvbook')
 def nvbook():
-    return render_template('nvbook.html')
+    return render_template('employees/nvbook.html')
 
 
 @app.route('/nvcheckin')
 def nvcheckin():
-    return render_template('nvcheckin.html')
+    return render_template('employees/nvcheckin.html')
 
 
 @app.route('/nvcheckout')
 def nvcheckout():
-    return render_template('nvcheckout.html')
+    return render_template('employees/nvcheckout.html')
+
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+
+@app.route('/reservation')
+def reservation():
+    return render_template('reservation.html')
 
 
 if __name__ == '__main__':
