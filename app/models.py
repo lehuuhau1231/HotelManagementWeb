d9d23639f47e53b5aee0995c0b76a5a5b4575e9c
trqhuong <quynhhuongtran314@gmail.com>
2025-06-12 22:38:15 +0700

    update

--- app/models.py
+++ app/models.py
@@ -161,7 +161,7 @@ rental_detail = db.Table('rental_detail',
 
 if __name__ == '__main__':
     with app.app_context():
-        db.drop_all()
+        # db.drop_all()
         db.create_all()
 
         customer_type1 = CustomerType(type='Domestic')
